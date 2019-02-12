14d15a0a98ec3392f18ad17d3140deddefcf2f84
Zhou You <yollyhideout@gmail.com>
2019-02-12 16:28:15 +0800

    Add test cases for coin

--- test/YollyCoin.js
+++ test/YollyCoin.js
@@ -1,15 +1,32 @@
 const YollyCoin = artifacts.require("../contracts/YollyCoin.sol");
+const LSMQueue = artifacts.require("../contracts/LSMQueue.sol");
 
 contract("YollyCoin", (accounts) => {
-    it("should be able to send direct settlement", () => {
+    
+    it("should be able to view balance", () => {
+        return YollyCoin.deployed().then(instance => {
+            instance.balanceOf(accounts[2])
+                .then( result => {
+                    assert.equal(result, 0);
+                })
+        });
+    })
+
+    it("should be able to directly settle if balance is sufficient", () => {
         return YollyCoin.deployed().then( instance => {
             instance.transfer(accounts[1], 10)
                 .then( result => {
                     assert.equal(result, true);
-                })
+                    return instance.balanceOf(accounts[1]);
+                }).then(result => {
+                    assert.equal(result, 10);
+                }).then(()=> {
+                    return instance.transferFrom(accounts[1], accounts[3], 5);
+                }).then(result => {
+                    assert.equal(result, true);
+                });
         });
     });
-
 
     it("should be able to enqueue using lsm", () => {
         return YollyCoin.deployed().then(instance => {
@@ -17,6 +34,24 @@ contract("YollyCoin", (accounts) => {
                 .then(result => {
                     assert.equal(result, true);
                 })
+        });
+    });    
+
+    it("should put txn into queue if balance is insufficient", () => {
+        return YollyCoin.deployed().then(instance => {
+            instance.balanceOf(accounts[1])
+                .then(result => {
+                    assert.equal(result, 5);
+                }).then(() => {
+                    return instance.transferFrom(accounts[1], accounts[2], 10);
+                }).then(result => {
+                    assert.equal(result, false);
+                    LSMQueue.deployed().then(instance => {
+                        return instance.getDepth();
+                    }).then(result => {
+                        assert.equal(result, 1);
+                    });
+                });
         });
     });    
 });
